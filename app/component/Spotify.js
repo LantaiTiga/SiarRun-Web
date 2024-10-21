dc1d1139bd4b6a8a062db2adf2b448e6a9c5124c
Alan-Alkalifa <work.alanalkalifa@gmail.com>
2024-10-21 21:52:47 +0700

    FINAL

--- app/component/Spotify.js
+++ app/component/Spotify.js
@@ -6,7 +6,7 @@ const Spotify = () => {
   return (
     <section className='flex flex-col items-center extra-light text-white text-center py-[18px] px-[23px] sm:px-[58px] md:px-[64px]'>
     <h1 className='text-[32px] py-[34px] sm:text-[48px]'>Run with the Rhythm</h1>
-    <a href='https://open.spotify.com/'>
+    <a href='https://open.spotify.com/playlist/0k38VEWRt9F2BVga9cZWWQ?si=w4dPIpNVSP6jO5UNrBx4kA&pi=a-_BG7w1tKQMSS'>
     <Image
     src={SoundIcon}
     alt="SoundWave"
